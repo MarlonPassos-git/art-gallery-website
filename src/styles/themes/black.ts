cbdd9135fa27f74556b8402ddb23a594999dce50
marlon Felipe Passos <1marlonpassos@gmail.com>
2021-12-14 13:02:33 -0300

    Adicionando 2 tipos de tema de cores para o footer

---
+++ src/styles/themes/black.ts
@@ -0,0 +1,7 @@
+const colors = {
+    background: 'var(--black)',
+    textColor: 'var(--white-light)',
+    hoverIcon: 'var(--orange)',
+}
+
+export default colors;
---
+++ src/styles/themes/orange.ts
@@ -0,0 +1,7 @@
+const colors = {
+    background: 'var(--orange)',
+    textColor: 'var(--black)',
+    hoverIcon: 'var(--white-light)',
+}
+
+export default colors;
